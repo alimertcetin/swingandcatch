ec6f4de10ece1408fa2538f8efb2ff7aeade3507
alimertcetin <mrtmert54@gmail.com>
2023-06-16 01:32:02 +0300

    Healthbar update

--- swingandcatch/Assets/Scripts/UISystems/HudHealthPageUI.cs
+++ swingandcatch/Assets/Scripts/UISystems/HudHealthPageUI.cs
@@ -10,6 +10,13 @@ namespace TheGame.UISystems
     public class HudHealthPageUI : PageUI
     {
         [SerializeField] Image image;
+        Color initialColor;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            initialColor = image.color;
+        }
 
         public void ChangeDisplayAmount(float newAmount)
         {
@@ -22,8 +29,12 @@ namespace TheGame.UISystems
             image.CancelTween();
             var current = image.fillAmount;
             var duration = 0.5f;
+            var currentColor = image.color;
+            var targetColor = Color.Lerp(Color.red, initialColor, newAmount);
             image.XIVTween()
                 .ImageFill(current, newAmount, duration, EasingFunction.Linear)
+                .And()
+                .ImageColor(currentColor, targetColor, duration, EasingFunction.Linear)
                 .Start();
         }
     }
