9345148d095d6b30356efaaee38cb4fd04425f1a
alimertcetin <mrtmert54@gmail.com>
2023-06-21 22:05:59 +0300

    Created FloatChannelSO

---
+++ swingandcatch/Assets/Scripts/ScriptableObjects/Channels/FloatChannelSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TheGame.ScriptableObjects.Channels
+{
+    [CreateAssetMenu(menuName = "Channels/FloatChannelSO")]
+    public class FloatChannelSO : XIVChannelSO<float>
+    {
+        
+    }
+}
