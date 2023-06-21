506eda6ffe3161e4548b131eecf06c3ebd1a4879
alimertcetin <mrtmert54@gmail.com>
2023-06-21 22:06:45 +0300

    MenuPath update

--- swingandcatch/Assets/Scripts/ScriptableObjects/MenuPaths.cs
+++ swingandcatch/Assets/Scripts/ScriptableObjects/MenuPaths.cs
@@ -2,11 +2,15 @@
 {
     public static class MenuPaths
     {
-        public const string FSM_BASE = "FSM/";
+        public const string BASE_MENU = "TheGame/ScriptableObjects/";
+        
+        public const string FSM_BASE = BASE_MENU + "FSM/";
         public const string PLAYER_FSM = FSM_BASE + "PlayerFSM/";
         public const string SAW_BLADE_FSM = FSM_BASE + "SawBladeFSM/";
 
         public const string PLAYER_FSM_STATE_DATA = PLAYER_FSM + "State Data/";
         public const string SAW_BLADE_FSM_STATE_DATA = SAW_BLADE_FSM + "State Data/";
+
+        public const string SCENE_MANAGEMENT_MENU = BASE_MENU + "Scene Management/";
     }
 }
