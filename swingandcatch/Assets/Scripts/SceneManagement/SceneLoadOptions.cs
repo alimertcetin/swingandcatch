430265747b5b627cef863fcd51c709a422557c74
alimertcetin <mrtmert54@gmail.com>
2023-06-22 02:18:47 +0300

    SceneLoadOptions moved to its own file

---
+++ swingandcatch/Assets/Scripts/SceneManagement/SceneLoadOptions.cs
@@ -0,0 +1,32 @@
+﻿namespace TheGame.SceneManagement
+{
+    public struct SceneLoadOptions
+    {
+        public int sceneToLoad;
+        public bool displayLoadingScreen;
+        public bool unloadActiveScene;
+        public bool activateImmediately;
+        
+        public static SceneLoadOptions MenuLoad(int sceneIndex)
+        {
+            return new SceneLoadOptions
+            {
+                sceneToLoad = sceneIndex,
+                displayLoadingScreen = false,
+                unloadActiveScene = true,
+                activateImmediately = true,
+            };
+        }
+
+        public static SceneLoadOptions LevelLoad(int sceneIndex)
+        {
+            return new SceneLoadOptions
+            {
+                sceneToLoad = sceneIndex,
+                displayLoadingScreen = true,
+                unloadActiveScene = true,
+                activateImmediately = false,
+            };
+        }
+    }
+}
--- swingandcatch/Assets/Scripts/ScriptableObjects/Channels/SceneLoadChannelSO.cs
+++ swingandcatch/Assets/Scripts/ScriptableObjects/Channels/SceneLoadChannelSO.cs
@@ -1,15 +1,8 @@
-﻿using UnityEngine;
+﻿using TheGame.SceneManagement;
+using UnityEngine;
 
 namespace TheGame.ScriptableObjects.Channels
 {
-    public struct SceneLoadOptions
-    {
-        public int sceneToLoad;
-        public bool displayLoadingScreen;
-        public bool unloadActiveScene;
-        public bool activateImmediately;
-    }
-    
     [CreateAssetMenu(menuName = "Channels/SceneLoadChannelSO")]
     public class SceneLoadChannelSO : XIVChannelSO<SceneLoadOptions>
     {
