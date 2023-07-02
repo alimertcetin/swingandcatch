ad1ebc6183a72003c4d30bf83d93c0b4fa780dcf
alimertcetin <mrtmert54@gmail.com>
2023-07-02 11:03:25 +0300

    Now player can attack to SawBlade

--- swingandcatch/Assets/Scripts/EnemySystems/SawBlade/SawBladeFSM.cs
+++ swingandcatch/Assets/Scripts/EnemySystems/SawBlade/SawBladeFSM.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using TheGame.EnemySystems.SawBlade.States;
 using TheGame.FSM;
+using TheGame.HealthSystems;
+using TheGame.ScriptableObjects.HealthSystems;
 using TheGame.ScriptableObjects.StateDatas.SawBladeStateDatas;
 using UnityEngine;
 using XIV.Core;
+using XIV.Core.TweenSystem;
 using XIV.Core.Utils;
 
 namespace TheGame.EnemySystems.SawBlade
 {
-    public class SawBladeFSM : StateMachine
+    public class SawBladeFSM : StateMachine, IDamageable
     {
+        [SerializeField] HealthSO healthSO;
+        [SerializeField] MeshRenderer heathbar;
         public SawBladeIdleStateDataSO idleStateDataSO;
         public SawBladeTransitionToIdleStateDataSO transitionToIdleStateDataSO;
         public SawBladeAttackStateDataSO attackStateDataSO;
@@ -17,17 +22,36 @@ namespace TheGame.EnemySystems.SawBlade
         [NonSerialized] public Vector3 idleStartPosition;
         public Vector3 idleEndPosition => idleStartPosition - idleStateDataSO.idleMovementAxis * idleStateDataSO.idleMovementDistance;
         public Collider2D collider2D { get; private set; }
+        public Health health;
 
         protected override void Awake()
         {
             idleStartPosition = transform.position + idleStateDataSO.idleMovementAxis.normalized * (idleStateDataSO.idleMovementDistance * 0.5f);
             collider2D = GetComponent<Collider2D>();
+            health = healthSO.GetHealth();
+            heathbar.material.SetFloat(ShaderConstants.Unlit_HealthbarShader.Health_RangeID, health.normalized);
             base.Awake();
         }
 
         protected override State GetInitialState()
         {
             return new SawBladeStateFactory(this).GetState<SawBladeIdleTransitionState>();
+        }
+
+        bool IDamageable.CanReceiveDamage() => health.isDepleted == false;
+
+        void IDamageable.ReceiveDamage(float amount)
+        {
+            health.DecreaseCurrentHealth(amount);
+            heathbar.material.SetFloat(ShaderConstants.Unlit_HealthbarShader.Health_RangeID, health.normalized);
+            if (health.isDepleted)
+            {
+                base.SetCurrentState(new EmptyState(this));
+                transform.XIVTween()
+                    .Scale(transform.localScale, Vector3.one * 0.5f, 0.5f, EasingFunction.EaseInOutBounce, true, 1)
+                    .OnComplete(() => Destroy(this.gameObject))
+                    .Start();
+            }
         }
 
 #if UNITY_EDITOR
