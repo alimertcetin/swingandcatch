68413f6136e45c493e6dca7c8624c9c973b07262
alimertcetin <mrtmert54@gmail.com>
2023-07-19 03:59:09 +0300

    XIVSerializer bug fix
    Idk why but had trouble with serialization in different project. It appears that JsonConvert will throw various exceptions if you wouldn't supply correct JsonSerializerSettings for it. Like "cant serialize abstract class" or "cant serialize value types" etc. This worked before lets hope that we won't get into trouble.

--- swingandcatch/Assets/Scripts/SaveSystems/XIVSerializer.cs
+++ swingandcatch/Assets/Scripts/SaveSystems/XIVSerializer.cs
@@ -4,19 +4,24 @@ namespace TheGame.SaveSystems
 {
     public static class XIVSerializer
     {
+        static readonly JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
+        {
+            TypeNameHandling = TypeNameHandling.All,
+        };
+        
         public static string Serialize(object deserializedData)
         {
-            return JsonConvert.SerializeObject(deserializedData, Formatting.Indented);
+            return JsonConvert.SerializeObject(deserializedData, Formatting.Indented, jsonSerializerSettings);
         }
 
         public static object Deserialize(string serializedData)
         {
-            return JsonConvert.DeserializeObject(serializedData);
+            return JsonConvert.DeserializeObject(serializedData, jsonSerializerSettings);
         }
 
         public static T Deserialize<T>(string serializedData)
         {
-            return JsonConvert.DeserializeObject<T>(serializedData);
+            return JsonConvert.DeserializeObject<T>(serializedData, jsonSerializerSettings);
         }
         
     }
