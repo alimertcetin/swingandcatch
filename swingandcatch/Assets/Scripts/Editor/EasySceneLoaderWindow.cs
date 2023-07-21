a4311a4d0e298a6fe3a46a08fd0b90f125d287f8
alimertcetin <mrtmert54@gmail.com>
2023-07-21 08:58:59 +0300

    EasySceneLoader update
    EasySceneLoader now easier to use
    - Features
     - Tracked folder count is not restricted by "Game Scenes" and "Test Scenes", you can track as many folders as you want
     - Track new folders by drag and drop
     - Solo display selected folder content

--- swingandcatch/Assets/Scripts/Editor/EasySceneLoaderWindow.cs
+++ swingandcatch/Assets/Scripts/Editor/EasySceneLoaderWindow.cs
@@ -1,66 +1,74 @@
-using System;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.SceneManagement;
 using XIV.XIVEditor.Utils;
 
-namespace LessonIsMath.XIVEditor.Windows
+namespace TheGame.Editor
 {
     public class EasySceneLoaderWindow : EditorWindow
     {
         bool isInitialized;
-        List<SceneAsset> scenes = new List<SceneAsset>();
-        List<SceneAsset> testScenes = new List<SceneAsset>();
+        
         Vector2 scenesScrollPos;
         Vector2 buttonsScrollPos;
+        
         bool additiveLoadToggle;
         Color additiveLoadToggleColor => additiveLoadToggle ? Color.white : Color.gray;
 
-        string sceneFolder;
-        string testFolder;
-        string searchStr;
+        Dictionary<string, List<SceneAsset>> scenes = new Dictionary<string, List<SceneAsset>>();
 
-        string sceneFolderKey => nameof(EasySceneLoaderWindow) + "_SceneFolderPath";
-        string testFolderKey => nameof(EasySceneLoaderWindow) + "_TestFolderPath";
+        const string sceneFolderPathSearchLiteral = nameof(EasySceneLoaderWindow) + "_SceneFolder_{0}";
+        const string sceneFolderCountKey = nameof(EasySceneLoaderWindow) + "_SceneFolderCount";
+        int sceneFolderCount;
 
         GUIStyle labelStyle;
+        
+        string searchStr;
         bool isSearching;
+
+        bool soloDisplay;
+        string soloDisplayFolder;
         
         [MenuItem("TheGame/Utilities/" + nameof(EasySceneLoaderWindow))]
         public static void ShowSceneLoaderWindow()
         {
-            EditorWindow.GetWindow<EasySceneLoaderWindow>("Easy Scene Loader").Show();
+            GetWindow<EasySceneLoaderWindow>("Easy Scene Loader").Show();
         }
 
         void OnEnable()
         {
-            sceneFolder = EditorPrefs.GetString(sceneFolderKey, "Assets/Scenes");
-            testFolder = EditorPrefs.GetString(testFolderKey, "Assets/Tests");
+            isInitialized = false;
         }
 
         void Initialize()
         {
-            AddScenes(sceneFolder, scenes);
-            AddScenes(testFolder, testScenes);
+            sceneFolderCount = EditorPrefs.GetInt(sceneFolderCountKey, 0);
+            AddAllScenes();
             labelStyle = EditorStyles.boldLabel;
             labelStyle.alignment = TextAnchor.MiddleCenter;
         }
 
         void OnProjectChange()
         {
-            scenes.Clear();
-            testScenes.Clear();
-            AddScenes(sceneFolder, scenes);
-            AddScenes(testFolder, testScenes);
+            SaveChanges();
+            AddAllScenes();
         }
 
         public override void SaveChanges()
         {
             base.SaveChanges();
-            EditorPrefs.SetString(sceneFolderKey, sceneFolder);
-            EditorPrefs.SetString(testFolderKey, testFolder);
+            int keysCount = scenes.Keys.Count;
+            EditorPrefs.SetInt(sceneFolderCountKey, keysCount);
+            sceneFolderCount = keysCount;
+            int index = 0;
+            foreach (string scenesKey in scenes.Keys)
+            {
+                EditorPrefs.SetString(string.Format(sceneFolderPathSearchLiteral, index++), scenesKey);
+            }
         }
 
         void OnDestroy()
@@ -76,102 +84,76 @@ namespace LessonIsMath.XIVEditor.Windows
                 isInitialized = true;
             }
 
-            Color tempGUIColor = GUI.color;
-            Color labelColor = Color.green;
+            HandleDragAndDrops();
 
-            GUILayout.Space(20f);
-            buttonsScrollPos = EditorGUILayout.BeginScrollView(buttonsScrollPos, false, false, 
-                GUI.skin.horizontalScrollbar, GUIStyle.none, GUI.skin.scrollView);
+            EditorGUILayout.Space(20f);
+            buttonsScrollPos = EditorGUILayout.BeginScrollView(buttonsScrollPos, false, false, GUILayout.ExpandHeight(false));
             buttonsScrollPos.y = 0f;
-            EditorGUILayout.BeginHorizontal();
-            GUI.color = additiveLoadToggleColor;
-            additiveLoadToggle = GUILayout.Toggle(additiveLoadToggle, "Load additive");
-            GUI.color = tempGUIColor;
-            if (GUILayout.Button("Select scene folder")) EditorUtils.HighlightOrCreateFolder(sceneFolder);
-            if (GUILayout.Button("Select test folder")) EditorUtils.HighlightOrCreateFolder(testFolder);
-            if (GUILayout.Button("Refresh")) OnProjectChange();
-            EditorGUILayout.EndHorizontal();
-            GUILayout.Space(20f);
-            EditorGUILayout.EndScrollView();
             
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Scene Folder Path");
-            sceneFolder = EditorGUILayout.TextField(sceneFolder);
-            EditorGUILayout.EndHorizontal();
-            
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Test Folder Path");
-            testFolder = EditorGUILayout.TextField(testFolder);
-            EditorGUILayout.EndHorizontal();
-            
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Search");
-            searchStr = EditorGUILayout.TextField(searchStr);
-            isSearching = string.IsNullOrWhiteSpace(searchStr) == false;
-            EditorGUILayout.EndHorizontal();
-            
-            GUILayout.Space(20f);
-            
-            scenesScrollPos = EditorGUILayout.BeginScrollView(scenesScrollPos, false, false);
 
-            if (isSearching)
+            EditorGUILayout.BeginHorizontal();
+            if (scenes.Count > 0) DrawLabel("Manage : ");
+            foreach (string folderPath in scenes.Keys)
             {
-                var allScenes = UnityEngine.Pool.ListPool<SceneAsset>.Get();
-                allScenes.AddRange(scenes);
-                allScenes.AddRange(testScenes);
-                int count = allScenes.Count;
-                for (int i = count - 1; i >= 0; i--)
+                var folderName = folderPath.Split('/')[^1];
+                if (GUILayout.Button(folderName))
                 {
-                    var sceneAsset = allScenes[i];
-                    if (sceneAsset.name.ToLower().Contains(searchStr.ToLower()) == false)
+                    bool isDisplayingSolo = soloDisplay && soloDisplayFolder == folderPath;
+                    var genericMenu = new GenericMenu();
+                    genericMenu.AddItem(new GUIContent("Highlight Folder"), false, () => EditorUtils.Highlight(folderPath));
+                    genericMenu.AddItem(new GUIContent("Display Solo"), isDisplayingSolo, () =>
                     {
-                        allScenes.RemoveAt(i);
-                        count--;
-                    }
+                        if (isDisplayingSolo)
+                        {
+                            soloDisplay = false;
+                            return;
+                        }
+                        DisplaySolo(folderPath);
+                    });
+                    genericMenu.AddItem(new GUIContent("Remove Folder"), false, () => RemoveFolder(folderPath));
+                    genericMenu.ShowAsContext();
                 }
-                
-                for (var i = 0; i < count; i++)
-                {
-                    SceneAsset sceneAsset = allScenes[i];
-                    GUILayout.Space(10);
-                    if (GUILayout.Button(sceneAsset.name, GUILayout.Height(50)) == false) continue;
+            }
+            
+            EditorGUILayout.EndHorizontal();
+            ColorUtility.TryParseHtmlString("#F3AA60", out var refreshButtonColor);
+            if (scenes.Count > 0 && DrawButton("Refresh", refreshButtonColor))
+            {
+                OnProjectChange();
+            }
+            EditorGUILayout.EndHorizontal();
+            
+            EditorGUILayout.EndScrollView();
 
-                    LoadScene(sceneAsset, additiveLoadToggle);
-                }
-                UnityEngine.Pool.ListPool<SceneAsset>.Release(allScenes);
+            if (scenes.Count > 0)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Label("Search : ");
+                searchStr = EditorGUILayout.TextField(searchStr);
+                isSearching = string.IsNullOrWhiteSpace(searchStr) == false;
+                EditorGUILayout.EndHorizontal();
+            }
+            
+            GUILayout.Space(20f);
+            if (scenes.Count > 0)
+            {
+                additiveLoadToggle = DrawToggle("Load additive", additiveLoadToggle, additiveLoadToggleColor);
+            }
+
+            scenesScrollPos = EditorGUILayout.BeginScrollView(scenesScrollPos, false, false, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
+            if (soloDisplay)
+            {
+                ColorUtility.TryParseHtmlString("#EF6262", out var soloDisplayLabelColor);
+                DrawLabel("SOLO : " + soloDisplayFolder.Split('/')[^1], labelStyle, soloDisplayLabelColor);
+                DisplayScenes(scenes[soloDisplayFolder]);
             }
             else
             {
-                GUI.color = labelColor;
-                if (scenes.Count > 0) GUILayout.Label("Game Scenes", labelStyle);
-                GUI.color = tempGUIColor;
-
-                for (var i = 0; i < scenes.Count; i++)
-                {
-                    SceneAsset sceneAsset = scenes[i];
-                    GUILayout.Space(10);
-                    if (GUILayout.Button(sceneAsset.name, GUILayout.Height(50)) == false) continue;
-
-                    LoadScene(sceneAsset, additiveLoadToggle);
-                }
-
-                GUILayout.Space(20f);
-
-                GUI.color = labelColor;
-                if (testScenes.Count > 0) GUILayout.Label("Test Scenes", labelStyle);
-                GUI.color = tempGUIColor;
-
-                for (var i = 0; i < testScenes.Count; i++)
-                {
-                    SceneAsset sceneAsset = testScenes[i];
-                    GUILayout.Space(10);
-                    if (GUILayout.Button(sceneAsset.name, GUILayout.Height(50)) == false) continue;
-
-                    LoadScene(sceneAsset, additiveLoadToggle);
-                }
+                DisplayAllScenes();
             }
-
             EditorGUILayout.EndScrollView();
+
             if (Event.current.isScrollWheel && Event.current.type != EventType.Used)
             {
                 float scrollAmount = Event.current.delta.y * 10f;
@@ -180,6 +162,102 @@ namespace LessonIsMath.XIVEditor.Windows
                 buttonsScrollPos.x = buttonsScrollPos.x < 0 ? 0 : buttonsScrollPos.x;
                 Event.current.Use();
             }
+        }
+
+        void HandleDragAndDrops()
+        {
+            if (scenes.Count == 0) DrawLabel("Drop folders to track scenes", labelStyle);
+            var addDropObjects = EditorUtils.HandleDragAndDrop();
+            if (addDropObjects != null)
+            {
+                foreach (object o in addDropObjects)
+                {
+                    if (o is SceneAsset || o is DefaultAsset)
+                    {
+                        AddFolder(AssetDatabase.GetAssetPath(o as Object));
+                    }
+                }
+            }
+        }
+
+        bool DrawButton(string text, Color c = default)
+        {
+            var prevCol = GUI.color;
+            GUI.color = c == default ? prevCol : c;
+            bool val = GUILayout.Button(text);
+            GUI.color = prevCol;
+            return val;
+        }
+
+        void DisplayAllScenes()
+        {
+            if (isSearching)
+            {
+                var filteredScenes = ListPool<SceneAsset>.Get();
+                foreach (List<SceneAsset> sceneAssets in scenes.Values)
+                {
+                    filteredScenes.AddRange(sceneAssets);
+                }
+
+                // Remove non-matching names from scene list
+                int count = filteredScenes.Count;
+                for (int i = count - 1; i >= 0; i--)
+                {
+                    if (filteredScenes[i].name.ToLower(CultureInfo.InvariantCulture).Contains(searchStr.ToLower(CultureInfo.InvariantCulture)) == false)
+                    {
+                        filteredScenes.RemoveAt(i);
+                    }
+                }
+
+                // Display current matching scenes
+                DisplayScenes(filteredScenes);
+
+                ListPool<SceneAsset>.Release(filteredScenes);
+            }
+            else
+            {
+                foreach (string folderPath in scenes.Keys)
+                {
+                    var folderName = folderPath.Split('/')[^1];
+                    DrawLabel(folderName, labelStyle, Color.green);
+                    DisplayScenes(scenes[folderPath]);
+                    GUILayout.Space(20f);
+                }
+            }
+        }
+
+        void DisplayScenes(List<SceneAsset> sceneList)
+        {
+            int sceneListCount = sceneList.Count;
+            for (int i = 0; i < sceneListCount; i++)
+            {
+                SceneAsset sceneAsset = sceneList[i];
+                GUILayout.Space(10);
+                if (GUILayout.Button(sceneAsset.name, GUILayout.Height(50)) == false) continue;
+
+                LoadScene(sceneAsset, additiveLoadToggle);
+            }
+        }
+
+        static bool DrawToggle(string text, bool value, Color color = default)
+        {
+            var prevColor = GUI.color;
+            color = color == default ? prevColor : color;
+            
+            GUI.color = color;
+            value = GUILayout.Toggle(value, text);
+            GUI.color = prevColor;
+            return value;
+        }
+
+        static void DrawLabel(string text, GUIStyle labelStyle = default, Color color = default)
+        {
+            labelStyle ??= EditorStyles.label;
+            var prevColor = GUI.color;
+            color = color == default ? prevColor : color;
+            GUI.color = color;
+            GUILayout.Label(text, labelStyle);
+            GUI.color = prevColor;
         }
 
         static void LoadScene(SceneAsset sceneAsset, bool additive)
@@ -204,11 +282,62 @@ namespace LessonIsMath.XIVEditor.Windows
             else EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(sceneAsset));
         }
 
-        void AddScenes(string path, List<SceneAsset> collection)
+        void AddAllScenes()
+        {
+            scenes.Clear();
+            for (int i = 0; i < sceneFolderCount; i++)
+            {
+                var key = string.Format(sceneFolderPathSearchLiteral, i);
+                var scenePath = EditorPrefs.GetString(key, null);
+                if (string.IsNullOrWhiteSpace(scenePath)) continue;
+                List<SceneAsset> sceneAssets = new List<SceneAsset>();
+                AddScenesToCollection(scenePath, sceneAssets);
+                if (sceneAssets.Count == 0)
+                {
+                    // key exists but there is no scene in the folder anymore
+                    EditorPrefs.DeleteKey(key);
+                    continue;
+                }
+                scenes.Add(scenePath, sceneAssets);
+            }
+        }
+
+        void AddScenesToCollection(string path, List<SceneAsset> collection)
         {
             if (System.IO.Directory.Exists(path) == false) return;
-            var valueCollection = AssetUtils.LoadAssetsOfType<SceneAsset>(path);
-            collection.AddRange(valueCollection);
+            var sceneAssets = AssetUtils.LoadAssetsOfType<SceneAsset>(path);
+            collection.AddRange(sceneAssets);
+        }
+
+        void DisplaySolo(string folderPath)
+        {
+            soloDisplay = true;
+            soloDisplayFolder = folderPath;
+        }
+
+        void AddFolder(string folderPath)
+        {
+            if (scenes.ContainsKey(folderPath))
+            {
+                var paths = scenes[folderPath];
+                paths.Clear();
+                AddScenesToCollection(folderPath, paths);
+                if (paths.Count > 0) OnProjectChange();
+                return;
+            }
+            var list = new List<SceneAsset>();
+            AddScenesToCollection(folderPath, list);
+            if (list.Count > 0)
+            {
+                scenes.Add(folderPath, list);
+                OnProjectChange();
+            }
+        }
+
+        void RemoveFolder(string folderPath)
+        {
+            scenes.Remove(folderPath);
+            OnProjectChange();
         }
     }
 }
